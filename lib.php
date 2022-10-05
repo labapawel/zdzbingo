6e9cd283d2e64633fab7406dcd73d79e9fb38fcc
unknown <labapawel@gmail.com>
2022-10-05 10:36:56 +0200

    env

--- lib.php
+++ lib.php
@@ -1,5 +1,13 @@
 <?php
 
 session_start();
-
-$db = new mysqli('localhost','','','');
+foreach(file('.env') as $item)
+    {
+        $env = explode("=", $item);
+        $_ENV[$env[0]]=$env[1];
+    }
+ 
+ 
+ 
+ 
+ $db = new mysqli('localhost',$_ENV['db'],$_ENV['pass'],$_ENV['db']);
